d9bec9b7356e7e2b0896de6fa35fb158b8223fc2
Kamil <kamilzajac@yandex.com>
2020-05-02 12:14:29 +0200

    updates to deploy to heroku

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,66 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'demo-oauth';
+export class AppComponent implements OnInit {
+  @ViewChild('loginRef', {static: true}) loginElement: ElementRef;
+  auth2: any;
+  id = '832386506095-qodt3b02teepj0mt9krcn8soqba6nqtf.apps.googleusercontent.com';
+
+  constructor() {
+  }
+
+  ngOnInit() {
+    this.googleSDK();
+  }
+
+  googleSDK() {
+
+    window['googleSDKLoaded'] = () => {
+      window['gapi'].load('auth2', () => {
+        this.auth2 = window['gapi'].auth2.init({
+          client_id: this.id,
+          cookiepolicy: 'single_host_origin',
+          scope: 'profile email'
+        });
+        this.prepareLoginButton();
+      });
+    };
+
+    ((d, s, id) => {
+      let js;
+      const fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) {
+        return;
+      }
+      js = d.createElement(s);
+      js.id = id;
+      js.src = 'https://apis.google.com/js/platform.js?onload=googleSDKLoaded';
+      fjs.parentNode.insertBefore(js, fjs);
+    })(document, 'script', 'google-jssdk');
+  }
+
+  prepareLoginButton() {
+
+    this.auth2.attachClickHandler(this.loginElement.nativeElement, {},
+      (googleUser) => {
+
+        const profile = googleUser.getBasicProfile();
+        console.log('Token || ' + googleUser.getAuthResponse().id_token);
+        console.log('ID: ' + profile.getId());
+        console.log('Name: ' + profile.getName());
+        console.log('Image URL: ' + profile.getImageUrl());
+        console.log('Email: ' + profile.getEmail());
+        //YOUR CODE HERE
+
+
+      }, (error) => {
+        alert(JSON.stringify(error, undefined, 2));
+      });
+
+  }
 }
+
